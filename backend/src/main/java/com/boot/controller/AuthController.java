a9df7b19f1f0b9a5109e8691f87ee9d74d05f5e4
NamTaeYoung <knnn47@naver.com>
2025-12-03 16:44:32 +0900

    비밀번호 재설정 부분 추가

--- backend/src/main/java/com/boot/controller/AuthController.java
+++ backend/src/main/java/com/boot/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.boot.controller;
 
 import com.boot.dto.LoginRequestDTO;
+import com.boot.dto.PasswordResetConfirmDTO;
+import com.boot.dto.PasswordResetRequestDTO;
 import com.boot.dto.RegisterRequestDTO;
 import com.boot.service.AuthService;
 import lombok.RequiredArgsConstructor;
@@ -21,17 +23,37 @@ public class AuthController {
         // 모든 로그인 로직은 서비스로 위임
         return authService.login(req);
     }
+    // 이메일 인증 확인
     @PostMapping("/check-email")
     public ResponseEntity<?> checkEmail(@RequestParam String email) {
         return authService.checkEmail(email);
     }
-
+    
+    //회원 가입
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterRequestDTO req) {
         return authService.register(req);
     }
+    
+    // 이메일 인증
     @GetMapping("/verify")
     public ResponseEntity<?> verifyEmail(@RequestParam String token) {
         return authService.verifyEmail(token);
+    }
+    
+    // 비밀번호 재설정
+    @PostMapping("/reset/request")
+    public ResponseEntity<?> requestPasswordReset(@RequestBody PasswordResetRequestDTO req) {
+        return authService.requestPasswordReset(req.getEmail());
+    }
+
+    @GetMapping("/reset/verify")
+    public ResponseEntity<?> verifyResetToken(@RequestParam String token) {
+        return authService.verifyResetToken(token);
+    }
+
+    @PostMapping("/reset/confirm")
+    public ResponseEntity<?> confirmReset(@RequestBody PasswordResetConfirmDTO req) {
+        return authService.resetPassword(req);
     }
 }
