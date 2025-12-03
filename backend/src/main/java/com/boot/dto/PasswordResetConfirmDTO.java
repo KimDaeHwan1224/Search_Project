5397300c59c5770dcaf0353d6949d64b4dfa01d0
NamTaeYoung <knnn47@naver.com>
2025-12-03 16:45:36 +0900

    비밀번호 재설정 DTO추가

---
+++ backend/src/main/java/com/boot/dto/PasswordResetConfirmDTO.java
@@ -0,0 +1,13 @@
+package com.boot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordResetConfirmDTO {
+    private String token;
+    private String newPassword;
+}
