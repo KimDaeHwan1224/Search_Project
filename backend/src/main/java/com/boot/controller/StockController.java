d5e0a7bbdd835428a5a85c0114df5881e500fcde
Rootplant <ho7465@gmail.com>
2025-12-03 17:23:50 +0900

    Update StockController.java

--- backend/src/main/java/com/boot/controller/StockController.java
+++ backend/src/main/java/com/boot/controller/StockController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 /*
  * 검색 + 상세
  * */
+@CrossOrigin(origins = "http://localhost:5173")
 public class StockController {
 
     private final StockInfoService stockInfoService;
