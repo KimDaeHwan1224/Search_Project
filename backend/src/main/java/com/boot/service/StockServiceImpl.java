2fc4a5b4d558ba018afb5e1c2c45d55b6ffde47a
ehdehf <ehdtjr5180@gmail.com>
2025-12-03 15:08:23 +0900

    Create StockServiceImpl.java

---
+++ backend/src/main/java/com/boot/service/StockServiceImpl.java
@@ -0,0 +1,25 @@
+package com.boot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.boot.dao.StockMapper;
+import com.boot.dto.StockInfoDTO;
+import com.boot.dto.StockNewsDTO;
+
+@Service
+public class StockServiceImpl implements StockService {
+
+    @Autowired
+    private StockMapper mapper;
+
+    @Override
+    public void insertStockInfo(StockInfoDTO dto) {
+        mapper.insertStockInfo(dto);
+    }
+
+    @Override
+    public void insertStockNews(StockNewsDTO dto) {
+        mapper.insertStockNews(dto);
+    }
+}
